344cc32b45d6afb54ade8b2df203f0f3d4c508f9
shimehituzi <shimehituzi@gmail.com>
2024-01-02 01:27:09 +0900

    set mappings for luasnip

--- lua/keymaps/nvim-cmp.lua
+++ lua/keymaps/nvim-cmp.lua
@@ -1,4 +1,6 @@
 local cmp = package.loaded.cmp
+local luasnip = package.loaded.luasnip
+
 return {
   mapping = {
     ["<C-j>"] = cmp.mapping(function()
@@ -7,27 +9,41 @@ return {
       else
         cmp.complete()
       end
-    end, {'i', 'c'}),
+    end, { 'i', 'c' }),
     ["<C-k>"] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_prev_item()
       else
         cmp.complete()
       end
-    end, {'i', 'c'}),
-    ['<CR>'] = cmp.mapping(function (fallback)
+    end, { 'i', 'c' }),
+    ['<CR>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
-        cmp.confirm()
+        cmp.confirm({ select = true })
       else
         fallback()
       end
-    end, {'i', 'c'}),
-    ['<Esc>'] = cmp.mapping(function (fallback)
+    end, { 'i', 'c' }),
+    ['<C-e>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.abort()
       else
         fallback()
       end
-    end, {'i', 'c'}),
+    end, { 'i', 'c' }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
   }
 }
