9a7ce3a9acdbcb72f80644234e0f0b2acaf70b8e
shimehituzi <shimehituzi@gmail.com>
2024-01-08 19:22:10 +0900

    copilot

--- lua/plugins/complete/nvim-cmp.lua
+++ lua/plugins/complete/nvim-cmp.lua
@@ -7,13 +7,30 @@ return {
     { "hrsh7th/cmp-path" },
     { "hrsh7th/cmp-cmdline" },
     {
-      "L3MON4D3/LuaSnip",
-      dependencies = { "rafamadriz/friendly-snippets" },
+      "saadparwaiz1/cmp_luasnip",
+      dependencies = {
+        "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+        end
+      },
+    },
+    {
+      "zbirenbaum/copilot-cmp",
+      dependencies = {
+        "zbirenbaum/copilot.lua",
+        config = function()
+          require("copilot").setup({
+            suggestion = { enabled = false },
+            panel = { enabled = false },
+          })
+        end
+      },
       config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
+        require("copilot_cmp").setup()
       end
     },
-    { "saadparwaiz1/cmp_luasnip" },
     { "onsails/lspkind.nvim" },
   },
   config = function()
@@ -27,6 +44,7 @@ return {
       },
       mapping = require('keymaps.nvim-cmp').mapping,
       sources = cmp.config.sources({
+        { name = "copilot" },
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'path' }
@@ -38,9 +56,28 @@ return {
         format = lspkind.cmp_format({
           mode = 'symbol_text',
           maxwidth = 50,
-          ellipsis_char = '...'
+          ellipsis_char = '...',
+          symbol_map = { Copilot = "" }
         })
-      }
+      },
+      sorting = {
+        priority_weight = 2,
+        comparators = {
+          require("copilot_cmp.comparators").prioritize,
+
+          -- Below is the default comparitor list and order for nvim-cmp
+          cmp.config.compare.offset,
+          -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.recently_used,
+          cmp.config.compare.locality,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
+      },
     })
 
     cmp.setup.cmdline('/', {
