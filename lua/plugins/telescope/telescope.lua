13344543007c92b811d887321e7c22c38b10d437
shimehituzi <shimehituzi@gmail.com>
2024-02-11 22:05:34 +0900

    change telescope sort

--- lua/plugins/telescope/telescope.lua
+++ lua/plugins/telescope/telescope.lua
@@ -19,18 +19,20 @@ return {
         mappings = {
           i = {
             ['<esc>'] = actions.close,
+            ['<C-q>'] = actions.close,
             ['<C-j>'] = actions.move_selection_next,
             ['<C-k>'] = actions.move_selection_previous,
             ['<C-t>'] = trouble.open_with_trouble,
           },
         },
         layout_strategy = "horizontal",
+        sorting_strategy = "ascending",
         layout_config = {
           horizontal = {
             width = 0.9,
             height = 0.9,
             preview_width = 0.6,
-            prompt_position = "bottom",
+            prompt_position = "top",
           },
         },
       },
