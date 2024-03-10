d4cea97d2bef22804ee830e0d83f9c9761a6466f
shimehituzi <shimehituzi@gmail.com>
2024-03-11 02:50:49 +0900

    add lsp server

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -14,7 +14,7 @@ return {
     require('neodev').setup()
     require('mason').setup()
     require('mason-lspconfig').setup({
-      ensure_installed = { 'lua_ls', 'tsserver', 'bashls' },
+      ensure_installed = { 'lua_ls', 'tsserver', 'bashls', 'html', 'eslint' },
     })
 
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
