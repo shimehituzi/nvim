c7b67668ec66517ebcafebb68988e576df4c686a
shimehituzi <shimehituzi@gmail.com>
2024-03-17 02:41:54 +0900

    split into files

--- lua/plugins/complete/nvim-cmp.lua
+++ lua/plugins/complete/nvim-cmp.lua
@@ -17,15 +17,7 @@ return {
     },
     {
       'zbirenbaum/copilot-cmp',
-      dependencies = {
-        'zbirenbaum/copilot.lua',
-        config = function()
-          require('copilot').setup({
-            suggestion = { enabled = false },
-            panel = { enabled = false },
-          })
-        end,
-      },
+      dependencies = 'zbirenbaum/copilot.lua',
       config = function() require('copilot_cmp').setup() end,
     },
     { 'onsails/lspkind.nvim' },
