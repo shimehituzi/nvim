8ec161973edff102573a68d7e475419875437467
shimehituzi <shimehituzi@gmail.com>
2024-03-17 22:48:47 +0900

    change copilot plugins

--- lua/plugins/llm/copilot.lua
+++ lua/plugins/llm/copilot.lua
@@ -2,8 +2,34 @@ return {
   'zbirenbaum/copilot.lua',
   config = function()
     require('copilot').setup({
-      suggestion = { enabled = false },
-      panel = { enabled = false },
+      panel = {
+        enabled = true,
+        auto_refresh = true,
+        keymap = {
+          jump_prev = '<up>',
+          jump_next = '<down>',
+          accept = '<left>',
+          refresh = '<right>',
+          open = '<C-g>',
+        },
+        layout = {
+          position = "right", -- | top | left | right
+          ratio = 0.4
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = false,
+        debounce = 75,
+        keymap = {
+          accept = false,
+          accept_word = false,
+          accept_line = false,
+          next = false,
+          prev = false,
+          dismiss = false,
+        },
+      },
     })
   end,
 }
