9e271ec37575a97ab7de0280d24681edca384c78
shimehituzi <shimehituzi@gmail.com>
2024-05-24 19:54:30 +0900

    change settings for copilot chat

--- lua/plugins/llm/copilot-chat.lua
+++ lua/plugins/llm/copilot-chat.lua
@@ -15,85 +15,59 @@ return {
         'The gitcommit block must be written in ENGLISH. Additionally, add an explanation of the gitcommit block. The explanation must be written in JAPANESE.'
 
       require('CopilotChat').setup({
-        debug = false,
-        proxy = nil,
-        allow_insecure = false,
-
         system_prompt = prompts.COPILOT_INSTRUCTIONS .. lang_prompt,
-        model = 'gpt-4',
-        temperature = 0.1,
-
-        name = 'CopilotChat',
-        separator = '---',
-        show_folds = true,
         show_help = false,
-        auto_follow_cursor = true,
-        auto_insert_mode = false,
-        clear_chat_on_new_prompt = false,
-
-        context = nil,
-        history_path = vim.fn.stdpath('data') .. '/copilotchat_history',
-        callback = nil,
-
-        selection = function(source) return select.visual(source) or select.buffer(source) end,
 
         prompts = {
           Explain = {
-            prompt = '/COPILOT_EXPLAIN Write an explanation for the code above as paragraphs of text.' .. lang_prompt,
+            prompt = '/COPILOT_EXPLAIN Write an explanation for the active selection as paragraphs of text.' .. lang_prompt,
           },
-          Tests = {
-            prompt = '/COPILOT_TESTS Write a set of detailed unit test functions for the code above.' .. lang_prompt,
+          Review = {
+            prompt = '/COPILOT_REVIEW Review the selected code.' .. lang_prompt,
           },
           Fix = {
-            prompt = '/COPILOT_FIX There is a problem in this code. Rewrite the code to show it with the bug fixed.' .. lang_prompt,
+            prompt = '/COPILOT_GENERATE There is a problem in this code. Rewrite the code to show it with the bug fixed.' .. lang_prompt,
           },
           Optimize = {
-            prompt = '/COPILOT_REFACTOR Optimize the selected code to improve performance and readablilty.' .. lang_prompt,
+            prompt = '/COPILOT_GENERATE Optimize the selected code to improve performance and readablilty.' .. lang_prompt,
           },
           Docs = {
-            prompt = '/COPILOT_REFACTOR Write documentation for the selected code. The reply should be a codeblock containing the original code with the documentation added as comments. Use the most appropriate documentation style for the programming language used (e.g. JSDoc for JavaScript, docstrings for Python etc.'
-              .. lang_prompt,
+            prompt = '/COPILOT_GENERATE Please add documentation comment for the selection.' .. lang_prompt,
+          },
+          Tests = {
+            prompt = '/COPILOT_GENERATE Please generate tests for my code.' .. lang_prompt,
           },
           FixDiagnostic = {
-            prompt = 'Please assist with the following diagnostic issue in file.' .. lang_prompt,
-            selection = select.diagnostics,
+            prompt = 'Please assist with the following diagnostic issue in file:' .. lang_prompt,
           },
           Commit = {
             prompt = 'Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.'
               .. lang_prompt_commit,
-            selection = select.gitdiff,
           },
           CommitStaged = {
             prompt = 'Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.'
               .. lang_prompt_commit,
-            selection = function(source) return select.gitdiff(source, true) end,
           },
         },
 
         window = {
           layout = 'float',
-          relative = 'editor',
           border = 'rounded',
           width = 0.8,
           height = 0.6,
-          row = nil,
-          col = nil,
-          title = 'Copilot Chat',
-          footer = nil,
-          zindex = 1,
         },
 
         mappings = {
           complete = {
-            detail = '',
-            insert = '<C-s>',
+            detail = nil,
+            insert = nil,
           },
           close = {
             normal = 'q',
             insert = nil,
           },
           reset = {
-            normal = '`',
+            normal = '<BS>',
             insert = nil,
           },
           submit_prompt = {
