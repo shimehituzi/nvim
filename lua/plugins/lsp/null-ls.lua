97a3d688e3e829d46433344e9b02ab75a85347e3
shimehituzi <shimehituzi@gmail.com>
2024-07-30 20:15:54 +0900

    prettier

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -9,10 +9,23 @@ return {
     },
   },
   config = function()
+    local null_ls = require('null-ls')
+    local prettier_configs = vim.tbl_map(
+      function(ext) return '.prettierrc' .. ext end,
+      { '', '.js', '.cjs', '.json', '.json5', '.yml', '.yaml', '.toml' }
+    ) or {}
+    table.insert(prettier_configs, 'prettier.config.js')
+    table.insert(prettier_configs, 'prettier.config.cjs')
+
     require('mason-null-ls').setup({
-      ensure_installed = { 'stylua', 'black' },
-      automatic_setup = true,
-      handlers = {},
+      ensure_installed = { 'stylua', 'black', 'prettier' },
+      handlers = {
+        prettier = function(source_name, methods)
+          null_ls.register(null_ls.builtins.formatting.prettier.with({
+            condition = function(utils) return utils.has_file(prettier_configs) end,
+          }))
+        end,
+      },
     })
   end,
 }
