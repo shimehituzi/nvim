cdcdf00dcd3d6fda89eb00256769b31b82a51bcb
shimehituzi <shimehituzi@gmail.com>
2024-12-29 14:05:06 +0900

    json ls

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -21,7 +21,7 @@ return {
   config = function()
     require('mason').setup()
     require('mason-lspconfig').setup({
-      ensure_installed = { 'lua_ls', 'ts_ls', 'bashls', 'html', 'gopls', 'hls', 'pyright', 'denols', 'rust_analyzer' },
+      ensure_installed = { 'lua_ls', 'ts_ls', 'bashls', 'html', 'gopls', 'hls', 'pyright', 'denols', 'rust_analyzer', 'jsonls' },
     })
 
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
