6ec339e2a172e240fbaba6eca5c8115105d7eaa5
shimehituzi <shimehituzi@gmail.com>
2024-12-31 15:43:11 +0900

    ignored specified warn mmessages

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -1,15 +1,38 @@
+local ignored_warn_messages = {
+  "position_encoding param is required in vim.lsp.util.make_position_params",
+  "multiple different client offset_encodings detected",
+}
+
+local function create_ignore_routes(messages)
+  local routes = {}
+  for _, m in ipairs(messages) do
+    table.insert(routes, {
+      filter = {
+        event = "notify",
+        find = m,
+      },
+      opts = {
+        skip = true,
+      },
+    })
+  end
+  return routes
+end
+
 return {
-  'folke/noice.nvim',
-  event = 'VeryLazy',
-  opts = {},
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    routes = create_ignore_routes(ignored_warn_messages),
+  },
   dependencies = {
-    'MunifTanjim/nui.nvim',
+    "MunifTanjim/nui.nvim",
     {
-      'rcarriga/nvim-notify',
+      "rcarriga/nvim-notify",
       config = function()
         ---@diagnostic disable-next-line: missing-fields
-        require('notify').setup({
-          stages = 'static',
+        require("notify").setup({
+          stages = "static",
         })
       end,
     },
