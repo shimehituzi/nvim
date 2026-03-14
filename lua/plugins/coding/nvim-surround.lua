f9c4fe1f1d8f1d0e30d84a3013285acc2786afab
shimehituzi <shimehituzi@gmail.com>
2026-03-14 23:07:13 +0900

    Simplifies nvim-surround configuration

--- lua/plugins/coding/nvim-surround.lua
+++ lua/plugins/coding/nvim-surround.lua
@@ -2,21 +2,10 @@ return {
   'kylechui/nvim-surround',
   version = '*', -- Use for stability; omit to use `main` branch for the latest features
   event = 'VeryLazy',
+  init = function()
+    vim.g.nvim_surround_no_mappings = true
+  end,
   config = function()
-    require('nvim-surround').setup({
-      keymaps = {
-        insert = false,
-        insert_line = false,
-        normal = false,
-        normal_cur = false,
-        normal_line = false,
-        normal_cur_line = false,
-        visual = false,
-        visual_line = false,
-        delete = false,
-        change = false,
-        change_line = false,
-      },
-    })
+    require('nvim-surround').setup()
   end,
 }
